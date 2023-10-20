709b2ae56aa09e025b5faacd8708d620688d3b2b
Lucas Fernandes Nogueira <lucas@tauri.app>
2023-10-20 16:51:46 +0000

    chore: update to tauri alpha.16, api alpha.9 (#673)
    Co-authored-by: Amr Bashir <amr.bashir2015@gmail.com>
    
    Committed via a GitHub action: https://github.com/tauri-apps/plugins-workspace/actions/runs/6590505706
    
    Co-authored-by: lucasfernog <lucasfernog@users.noreply.github.com>

--- dist-js/index.mjs
+++ dist-js/index.mjs
@@ -1,14 +1,16 @@
+import { invoke } from '@tauri-apps/api/primitives';
+
 // Copyright 2019-2023 Tauri Programme within The Commons Conservancy
 // SPDX-License-Identifier: Apache-2.0
 // SPDX-License-Identifier: MIT
 async function isEnabled() {
-    return await window.__TAURI_INVOKE__("plugin:autostart|is_enabled");
+    return await invoke("plugin:autostart|is_enabled");
 }
 async function enable() {
-    await window.__TAURI_INVOKE__("plugin:autostart|enable");
+    await invoke("plugin:autostart|enable");
 }
 async function disable() {
-    await window.__TAURI_INVOKE__("plugin:autostart|disable");
+    await invoke("plugin:autostart|disable");
 }
 
 export { disable, enable, isEnabled };
